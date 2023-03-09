0e6fdb3ebd8ee12813a1dc6701e10baba33c8965
Bnyro <bnyro@tutanota.com>
2023-03-09 14:49:57 +0100

    Spinner UI improvements

--- src/components/Details.svelte
+++ src/components/Details.svelte
@@ -1,86 +1,86 @@
-<div id="details">
-{#if details}
-    <img style="display: none;" src="" alt="">
-    <button id="x" on:click={onDestroy}>X</button>
-    <article>
-        <h1>{details.title}</h1>
-        <div id="content">{@html content}</div>
-    </article>
-{:else}
-    <Spinner />
-{/if}
-</div>
-
 <script lang="ts">
-	import fetchJson from "../lib/fetch";
-	import { onMount } from "svelte";
-    import Spinner from "./Spinner.svelte"
-	import generateContentHtml from "$lib/parser";
+	import fetchJson from '../lib/fetch';
+	import { onMount } from 'svelte';
+	import Spinner from './Spinner.svelte';
+	import generateContentHtml from '$lib/parser';
 
-    export let url: string;
-    export let onDestroy: () => void;
+	export let url: string;
+	export let onDestroy: () => void;
 
-    let details: any = null;
-    let content: string;
+	let details: any = null;
+	let content: string;
 
-    const fetchDetails = async () => {
-        details = await fetchJson(url);
-        content = generateContentHtml(details.content);
-    }
+	const fetchDetails = async () => {
+		details = await fetchJson(url);
+		content = generateContentHtml(details.content);
+	};
 
-    onMount(() => {
-        fetchDetails();
-    })
+	onMount(() => {
+		fetchDetails();
+	});
 </script>
 
+<div id="details">
+	{#if details}
+		<img style="display: none;" src="" alt="" />
+		<button id="x" on:click={onDestroy}>X</button>
+		<article>
+			<h1>{details.title}</h1>
+			<div id="content">{@html content}</div>
+		</article>
+	{:else}
+		<Spinner />
+	{/if}
+</div>
+
 <style>
-    #details {
-        position: fixed;
-        height: 100vh;
-        width: 100vw;
-        max-width: 100%;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background: var(--bg);
-        z-index: 100;
-    }
+	#details {
+		position: fixed;
+		height: 100vh;
+		width: 100vw;
+		max-width: 100%;
+		left: 0;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background: var(--bg);
+		z-index: 100;
+	}
 
-    #x {
-        border-radius: 50%;
-        background: var(--highlight);
-        color: var(--text);
-        width: 2rem;
-        height: 2rem;
-        position: absolute;
-        right: 0;
-        top: 0;
-        margin: 2rem;
-        cursor: pointer;
-        transition: all ease-in-out 0.2s;
-    }
+	#x {
+		border-radius: 50%;
+		background: var(--highlight);
+		color: var(--text);
+		width: 2rem;
+		height: 2rem;
+		position: absolute;
+		right: 0;
+		top: 0;
+		margin: 2rem;
+		cursor: pointer;
+		transition: all ease-in-out 0.2s;
+	}
 
-    #x:hover {
-        opacity: 0.7;
-    }
+	#x:hover {
+		opacity: 0.7;
+	}
 
-    article {
-        padding: 2rem 10vw;
-        text-align: center;
-        overflow-y: scroll;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+	article {
+		padding: 2rem 10vw;
+		text-align: center;
+		overflow-y: scroll;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-    @media only screen and (max-width: 800px) {
-        article {
-            padding: 2rem 1rem;
-        }
-    }
+	@media only screen and (max-width: 800px) {
+		article {
+			padding: 2rem 1rem;
+		}
+	}
 </style>
--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -1,31 +1,35 @@
 <div class="spin-container">
-    <div class="spin"></div>
+	<div class="spin" />
 </div>
 
 <style>
-.spin-container{
-    box-sizing: content-box;
-    background: -webkit-linear-gradient(left top, crimson 0%, blue 100%);
-    width: var(--progress-size);
-    height: var(--progress-size);
-    border-radius: 50%;
-    padding: calc(var(--progress-size) / 10);
-    animation: spin infinite linear 2s;
-  }
-  
-.spin{
-    background: var(--bg);
-    width: var(--progress-size);
-    height: var(--progress-size);
-    border-radius: 50%;
-}
+	:root {
+		--progress-size: 3rem;
+	}
 
-@keyframes spin {
-    from {
-        rotate: 0deg;
-    }
-    to {
-        rotate: 360deg;
-    }
-}
+	.spin-container {
+		box-sizing: content-box;
+		background: -webkit-linear-gradient(left top, var(--primary) 0%, var(--secondary) 100%);
+		width: var(--progress-size);
+		height: var(--progress-size);
+		border-radius: 50%;
+		padding: calc(var(--progress-size) / 10);
+		animation: spin infinite linear 2s;
+	}
+
+	.spin {
+		background: var(--bg);
+		width: var(--progress-size);
+		height: var(--progress-size);
+		border-radius: 50%;
+	}
+
+	@keyframes spin {
+		from {
+			rotate: 0deg;
+		}
+		to {
+			rotate: 360deg;
+		}
+	}
 </style>
