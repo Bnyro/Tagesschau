575b9aeadcda7c8effba99cb15e8a28e12c19357
Bnyro <bnyro@tutanota.com>
2023-03-09 14:51:17 +0100

    Minor code cleanup

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,32 +1,33 @@
 export default function generateContentHtml(content: any) {
-    let html: string[] = [];
-    for (var i = 0; i < content.length; i++) {
-        const part = content[i];
-        if (part.type == "text" || part.type == "headline") {
-            html.push(part.value);
-        } else if (part.type == "box" && part.box.images != undefined) {
-            const box = part.box;
-            html.push(
+	let html: string[] = [];
+	for (var i = 0; i < content.length; i++) {
+		const part = content[i];
+		if (part.type == 'text' || part.type == 'headline') {
+			html.push(part.value);
+		} else if (part.type == 'box' && part.box.images != undefined) {
+			html.push(
+				`
+                <img src="${part.box.images.videowebl.imageurl ?? ''}" alt="${
+					part.box.images.alttext ?? ''
+				}" style="margin: 2rem 0; border-radius: 1rem; max-width: 90%;">
+                <figcaption>${part.box.title ?? ''}</figcaption>
                 `
-                <img src="${box.images.videowebl.imageurl ?? ''}" alt="${box.images.alttext ?? ''}" style="margin: 2rem 0; border-radius: 1rem; max-width: 90%;">
-                <figcaption>${box.title ?? ""}</figcaption>
-                `
-            )
-        } else if (part.type == "video") {
-            html.push(
-                `
+			);
+		} else if (part.type == 'video') {
+			html.push(
+				`
                 <video controls style="margin: 2rem 0; border-radius: 1rem;">
                     <source src="${part.video.streams.h264m ?? part.video.streams.h264xl}">    
                 </video>
                 `
-            )
-        } else if (part.type == "htmlEmbed") {
-            html.push(
-                `
+			);
+		} else if (part.type == 'htmlEmbed') {
+			html.push(
+				`
                 <embed src="${part.htmlEmbed.url}" style="width: min(90%, 50rem); height: 30rem; border-radius: 1rem;">
                 `
-            )
-        }
-    }
-    return html.join("");
+			);
+		}
+	}
+	return html.join('');
 }
