f004abf3882fea0dc97a9ca62df82dbd6afa45b7
Bnyro <bnyro@tutanota.com>
2023-09-13 11:02:41 +0200

    feat: UI support for filtering by region and ressort

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,33 @@
 	import { onMount } from 'svelte';
 	import Article from '../components/Article.svelte';
 	import { page } from '$app/stores';
+	import { goto, invalidateAll } from '$app/navigation';
 
 	let news: any[] = [];
 	let homepage: any[] = [];
 	let regional: any[] = [];
 	let nextPage: string | null = null;
+
+	const ressorts = ['alle', 'inland', 'ausland', 'wirtschaft', 'sport', 'investigativ']; // 'video' ressort not yet supported
+	const regions = [
+		'Alle',
+		'Baden-Württemberg',
+		'Bayern',
+		'Berlin',
+		'Brandenburg',
+		'Bremen',
+		'Hamburg',
+		'Hessen',
+		'Mecklenburg-Vorpommern',
+		'Niedersachsen',
+		'Nordrhein-Westfalen',
+		'Rheinland-Pfalz',
+		'Saarland',
+		'Sachsen',
+		'Sachsen-Anhalt',
+		'Schleswig-Holstein',
+		'Thüringen'
+	];
 
 	const filterArticles = (articles: any[]) => {
 		return articles.filter((article) => article.type == 'story');
@@ -44,13 +66,37 @@
 		});
 	};
 
-	onMount(async () => {
+	const refreshPage = async () => {
+		homepage = [];
+		regional = [];
+		news = [];
 		const regions = $page.url.searchParams.get('regions');
 		const ressort = $page.url.searchParams.get('ressort');
 		if (!regions && !ressort) await fetchHomePage();
 		fetchNews(regions, ressort);
+	};
+
+	onMount(() => {
+		refreshPage();
 		createScrollListener();
 	});
+
+	const refreshWithNewParams = (key: string, value: any | null) => {
+		const params = $page.url.searchParams;
+		if (value) params.set(key, value);
+		else params.delete(key);
+		goto('./?' + params);
+		refreshPage();
+	};
+
+	const onRessortClick = (ressort: string) => {
+		refreshWithNewParams('ressort', ressort != 'alle' ? ressort : null);
+	};
+
+	const onRegionClick = (region: string) => {
+		const regionIndex = regions.indexOf(region);
+		refreshWithNewParams('regions', regionIndex != 0 ? regionIndex : null);
+	};
 </script>
 
 <svelte:head>
@@ -58,6 +104,16 @@
 </svelte:head>
 
 <section id="news">
+	<div id="ressorts">
+		{#each ressorts as ressort}
+			<button on:click={() => onRessortClick(ressort)}>{ressort}</button>
+		{/each}
+	</div>
+	<div id="regions">
+		{#each regions as region}
+			<button on:click={() => onRegionClick(region)}>{region}</button>
+		{/each}
+	</div>
 	{#if homepage.length}
 		<h1>Homepage</h1>
 		{#each homepage as article}
@@ -88,5 +144,22 @@
 	h1 {
 		margin-top: 3rem;
 		margin-left: 0.5rem;
+	}
+
+	#regions {
+		margin-top: 1rem;
+	}
+
+	#ressorts button,
+	#regions button {
+		margin: 0.4rem;
+		padding: 0.5rem 0.7rem;
+		border-radius: 0.5rem;
+		outline: none;
+		border: none;
+		text-transform: capitalize;
+		background: var(--accent);
+		color: var(--bg);
+		cursor: pointer;
 	}
 </style>
